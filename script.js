53d608912211997135024ea840c8c02a1220441f
ninjuh1124 <32426314+ninjuh1124@users.noreply.github.com>
2022-04-03 06:08:54 -0400

    Update script.js

--- script.js
+++ script.js
@@ -3,13 +3,20 @@
 // @namespace    http://tampermonkey.net/
 // @match        https://hot-potato.reddit.com/embed*
 // ==/UserScript==
+
+
+/*** TOP LEFT X COORDINATE ***/
+var topLeftX = 1290;
+/*** TOP LEFT Y COORDINATE ***/
+var topLeftY = 664;
+
 if (window.top !== window.self) {
     window.addEventListener('load', () => {
             document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
         (function () {
             const i = document.createElement("img");
-            i.src = "https://raw.githubusercontent.com/ninjuh1124/hyouka-place-overlay/master/hyoukaplace.png";
-            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            i.src = "https://raw.githubusercontent.com/ninjuh1124/raw/master/hyoukaplaceraw.png?" + Date.now();
+            i.style = "position: absolute;left: " + topLeftX + "px;top: " + topLeftY + "px;image-rendering: pixelated;width: 32px;height: 9px;";
             console.log(i);
             return i;
         })())
