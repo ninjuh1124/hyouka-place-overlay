e6612226707ac2247edf50768624eef4d9813f10
ninjuh1124 <32426314+ninjuh1124@users.noreply.github.com>
2022-04-03 05:22:25 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name         hyouka Logo template
+// @namespace    http://tampermonkey.net/
+// @match        https://hot-potato.reddit.com/embed*
+// ==/UserScript==
+if (window.top !== window.self) {
+    window.addEventListener('load', () => {
+            document.getElementsByTagName("mona-lisa-embed")[0].shadowRoot.children[0].getElementsByTagName("mona-lisa-canvas")[0].shadowRoot.children[0].appendChild(
+        (function () {
+            const i = document.createElement("img");
+            i.src = "https://raw.githubusercontent.com/ninjuh1124/raw/master/hyoukaplace.png";
+            i.style = "position: absolute;left: 0;top: 0;image-rendering: pixelated;width: 2000px;height: 1000px;";
+            console.log(i);
+            return i;
+        })())
+
+    }, false);
+
+}
